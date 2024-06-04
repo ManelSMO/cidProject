b9da1cfd57155a423177047a108252ff4ba8f620
Emanuel Previatti <emanuelpreviatti@gmail.com>
2024-06-03 21:43:10 -0300

    Script de drop de tabelas

---
+++ scriptDropTabelas.sql
@@ -0,0 +1,18 @@
+drop table pessoa CASCADE;
+
+drop table funcionario CASCADE;
+
+drop table departamentopolicia;
+
+drop table ocorrencia;
+
+drop table tipo_ocorrencia;
+
+drop table bo;
+
+drop table endereco CASCADE;
+
+drop table cidade;
+
+drop table estado;
+
