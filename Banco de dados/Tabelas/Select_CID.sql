b62ad4b12b31fdc7051b4ee8efce9cec37d4d5a6
Luiz-Przygoda <luizprzygoda24@gmail.com>
2024-06-05 22:20:56 -0300

    Selects para consulta do Banco de Dados

---
+++ Banco de dados/Tabelas/Select_CID.sql
@@ -0,0 +1,26 @@
+SELECT 
+    codDP AS "Código do Departamento", 
+    distritoDP AS "Distrito"
+FROM 
+    DepartamentoPolicia;
+   
+   SELECT 
+    codPessoa AS "Código da Pessoa", 
+    nomPessoa AS "Nome da Pessoa"
+FROM 
+    Pessoa;
+   
+   SELECT 
+    codtipocorrencia AS "Código do Tipo de Ocorrência", 
+    descOcorencia AS "Descrição"
+FROM 
+    tipo_Ocorrencia;
+
+   SELECT 
+    codBO AS "Código do BO", 
+    dataBO AS "Data do BO", 
+    localBO AS "Local do BO"
+FROM 
+    BO;
+
+
