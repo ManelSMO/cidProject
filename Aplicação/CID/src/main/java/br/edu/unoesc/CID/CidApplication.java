07eb6d668da2a1d244556494e09c26536296ee9b
Emanuel <emanuelpreviatti@gmail.com>
2024-11-03 20:31:54 -0300

    Primeira versao dos arquivos CID

---
+++ Aplicação/CID/src/main/java/br/edu/unoesc/CID/CidApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.CID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidApplication.class, args);
+	}
+
+}
---
+++ Aplicação/CID/src/test/java/br/edu/unoesc/CID/CidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.CID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
